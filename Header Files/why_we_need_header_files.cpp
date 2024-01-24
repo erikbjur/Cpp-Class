22b8b538311a2e3defabe342a539baf81c3a4492
Erik Bjur <erik@bjurengineering.com>
2024-01-23 21:48:39 -0800

    Added example on why we need header files

---
+++ Header Files/why_we_need_header_files.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    // Lets use the functions above to do math
+    int i_first_number = 5;
+    int i_second_number = 2;
+
+    // Here we add the two int variables using the int version of Add()
+    std::cout << i_first_number << " + " 
+        << i_second_number << " = " 
+        << MathFunctions::Add(i_first_number, i_second_number) << std::endl;
+    
+    return 0;
+}
+
+// Here's the math functions we need above.
+namespace MathFunctions {
+    void PrintAddingInts() {
+        std::cout << "Adding ints using Add(int a, int b)" << std::endl;
+    }
+
+    int Add(int a, int b){
+        return a + b;
+    }
+}
